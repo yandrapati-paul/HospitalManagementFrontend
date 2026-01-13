8f65cec5db560a9d973dc9426cf40d15ffec5fd8
yandrapati-paul <yandrapatinishipaul@gmail.com>
2026-01-13 14:06:41 +0530

    third commit

--- visitadd.js
+++ visitadd.js
@@ -2,40 +2,52 @@
 async function doctorSubmit(){
 
 
-    let newapi='https://d1dgqwnu0ivu2l.cloudfront.net/visits';
+    let newapi='http://hmsmanagement9.ap-southeast-2.elasticbeanstalk.com/visits';
 
-    const today = new Date();
-    let day = today.getDate();
-    let month = today.getMonth() + 1;
-    let year = today.getFullYear();
-    let formattedDate = `${day}-${month}-${year}`;
+  const today = new Date();
+  let day = String(today.getDate()).padStart(2, "0");
+  let month = String(today.getMonth() + 1).padStart(2, "0");
+  let year = today.getFullYear();
+  let visitDateStr = `${day}-${month}-${year}`;
 
-    let visitDateStr=formattedDate;
+
     let diagnosis=document.getElementsByName("diagnosis")[0].value;
-    let wtKg=document.getElementsByName("wtKg")[0].value;
-    let tempC=document.getElementsByName("tempC")[0].value;
-    let bpValue=document.getElementsByName("bpValue")[0].value;
-    let paymentType=document.getElementsByName("paymentType")[0].value;
-    let pid=document.getElementsByName("pid")[0].value;
-    let did=document.getElementsByName("did")[0].value;
+    let wtKg = parseFloat(document.getElementsByName("wtKg")[0].value);
+    let tempC = parseFloat(document.getElementsByName("tempC")[0].value);
+    let bpValue = parseFloat(document.getElementsByName("bpValue")[0].value);
+    let pid = parseInt(document.getElementsByName("pid")[0].value);
+    let did = parseInt(document.getElementsByName("did")[0].value);
 
-
-    let obj={
-      visitDateStr: visitDateStr,
-      diagnosis: diagnosis,
-      wtKg: Number(wtKg),
-      tempC: Number(tempC),
-      bpValue: Number(bpValue),
-      paymentType: paymentType,
-      pid: Number(pid),
-      did: Number(did),
+    if (isNaN(wtKg) || isNaN(tempC) || isNaN(bpValue) || isNaN(pid) || isNaN(did)) {
+      alert("Please fill all numeric fields correctly");
+      return;
     }
+
+    let paymentType=document.getElementsByName("paymentType")[0].value;
+
+
+let obj = {
+  visitDateStr: visitDateStr,
+  diagnosis: diagnosis,
+  wtKg: Number(wtKg),
+  tempC: Number(tempC),
+  bpValue: Number(bpValue),
+  paymentType: paymentType,
+  pid: Number(pid),
+  did: Number(did)
+};
+
 
     let modalElem=document.getElementById("exampleModalLabel");
     let modalBodyElem=document.getElementById("modal-body");
 
     try{
-      let res=await axios.post(newapi,obj);
+      let res = await axios.post(newapi, obj, {
+  headers: {
+    "Content-Type": "application/json"
+  }
+});
+
       modalElem.innerText="Sucess";
       modalBodyElem.innerHTML=`<p>visit between doc: ${did} and patient: ${pid} has successfully added</p>`;
     }
